d6789c4c4a95db802a51a2d86e7ce8a2b2d65c51
Fakhri <fakhri.aunur@gmail.com>
2024-10-13 13:05:23 +0700

    Add ParentNode

---
+++ src/test_parentnode.py
@@ -0,0 +1,13 @@
+import unittest
+
+from parentnode import ParentNode
+
+class TestParentNode(unittest.TestCase):
+    def test_eq_values(self):
+        pass
+    
+    def test_eq_repr(self):
+        pass
+    
+    def test_eq_to_html(self):
+        pass
