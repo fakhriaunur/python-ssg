eaef8ecefdd77fd265904ea1c3c3d5d35273ca62
Fakhri <fakhri.aunur@gmail.com>
2024-10-13 12:41:24 +0700

    Add LeafNode

---
+++ src/test_leafnode.py
@@ -0,0 +1,10 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_eq_values(self):
+        pass
+    
+    def test_to_html(self):
+        pass
