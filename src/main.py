7601baa0e6116bca039f9d9b6cd665a2df1f81fa
Fakhri <fakhri.aunur@gmail.com>
2024-10-13 22:04:32 +0700

    add textnode to htmlnode function

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
-from textnode import TextNode
+from textnode import *
 from htmlnode import HTMLNode
+from textnode import text_node_to_html_node
 
 def main():
     print("hello world")
     
-    text_node = TextNode("hello world", "bold", "https://boot.dev")
+    text_node = TextNode("hello world", text_type_link, "https://boot.dev")
     print(text_node)
     
     html_node = HTMLNode("p", "new paragraph")
     print(html_node)
+    
+    html_node_result = text_node_to_html_node(text_node)
+    print(html_node_result)
 
 if __name__ == "__main__":
     main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,11 +1,19 @@
 import unittest
 
-from textnode import TextNode
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_link,
+    text_type_image
+)
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+        node = TextNode("This is a text node",text_type_bold)
+        node2 = TextNode("This is a text node",text_type_bold)
         
         self.assertEqual(node, node2)
     
@@ -24,6 +32,9 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(This is a text node, bold, https://boot.dev)", repr(node)
         )
+    
+    def test_eq_textnode_to_htmlnode(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
